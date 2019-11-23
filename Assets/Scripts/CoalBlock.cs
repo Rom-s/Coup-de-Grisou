6297d36ad6f4c015efa79b4f35c0ec960c7553fd
louis lafuma <louis.lafuma@ensiie.fe>
2019-11-23 10:46:05 +0100

    add player control + coal mining
    -add a script to control the character (need the character controller component
    -add a script to turn block into destructible coal block

---
+++ Assets/Scripts/CoalBlock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoalBlock : MonoBehaviour
+{
+    private float BlockLife = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(BlockLife == 0)
+        {
+            Debug.Log("Block Destroyed");
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    public void MineBlock()
+    {
+        BlockLife -= 1;
+        Debug.Log("MiningBlock");
+    }
+}
